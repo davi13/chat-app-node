fae6c30d724cbbcf91d0a2ac2d2baa1bee08be23
Daviel Frias Garcia <yfriasgarcia@gmail.com>
2019-09-21 22:34:18 +0200

    added the public folder to render our page html, and create our path in express to render the html file

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
+const path = require('path');
 const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('hello express');
-});
+const port = process.env.PORT || 3000
 
-app.listen(3000, () => {
-    console.log('Server is up on port 3000.')
+const publicDirectoryPath = path.join(__dirname, '../public');
+
+app.use(express.static(publicDirectoryPath));
+
+
+
+app.listen(port, () => {
+    console.log(`Server is up on port ${port}`)
 })
