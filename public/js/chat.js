cf406d942bb98fc75ffdbb91108d98c0b776d11f
Daviel Frias Garcia <yfriasgarcia@gmail.com>
2019-09-22 22:11:35 +0200

    public/js/chat.js sending un message sended in one browser to all user

--- public/js/chat.js
+++ public/js/chat.js
@@ -4,7 +4,9 @@ socket.on('message', (message) => {
     console.log(message)
 });
 
-// document.querySelector('#increment').addEventListener('click', () => {
-//     console.log('clicked');
-//     socket.emit('increment');
-// })
+document.querySelector('#sendMessage').addEventListener('submit', (e) => {
+    e.preventDefault();
+    console.log(e.target.elements.message.value);
+    e.target.elements.message.value = ''
+    socket.emit(e.target.elements.message.value);
+})
