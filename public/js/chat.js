65bd66e4a609730a87f41c6569274d73cc867cee
Daviel Frias Garcia <yfriasgarcia@gmail.com>
2019-09-23 10:38:28 +0200

    public/js/chat.js sending un message sended in one browser to all user V2 make a small tweak in the way we get the data from user

--- public/js/chat.js
+++ public/js/chat.js
@@ -8,8 +8,8 @@ document.querySelector('#message-form').addEventListener('submit', (e) => {
     e.preventDefault();
     //console.log(e.target.elements.message.value);
     //e.target.elements.message.value = ''
-    const message = document.querySelector('input').value
-    message.value = ''
+    const message = e.target.elements.message.value
+    e.target.elements.message.value = ''
     //socket.emit(e.target.elements.message.value);
     socket.emit('sendMessage', message);
 })
