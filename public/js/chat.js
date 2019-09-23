4bf23af2d7f1621b4bd0057cd6132bc90b0804f3
Daviel Frias Garcia <yfriasgarcia@gmail.com>
2019-09-23 10:04:06 +0200

    public/js/chat.js sending un message sended in one browser to all user V2

--- public/js/chat.js
+++ public/js/chat.js
@@ -4,9 +4,12 @@ socket.on('message', (message) => {
     console.log(message)
 });
 
-document.querySelector('#sendMessage').addEventListener('submit', (e) => {
+document.querySelector('#message-form').addEventListener('submit', (e) => {
     e.preventDefault();
-    console.log(e.target.elements.message.value);
-    e.target.elements.message.value = ''
-    socket.emit(e.target.elements.message.value);
+    //console.log(e.target.elements.message.value);
+    //e.target.elements.message.value = ''
+    const message = document.querySelector('input').value
+    message.value = ''
+    //socket.emit(e.target.elements.message.value);
+    socket.emit('sendMessage', message);
 })
--- src/index.js
+++ src/index.js
@@ -18,8 +18,8 @@ io.on('connection', (socket) => {
     console.log('New Websocket connection');
     socket.emit('message', 'Welcome')
 
-    socket.on('print', () => {
-        io.emit('message', 'Welcome');
+    socket.on('sendMessage', (message) => {
+        io.emit('message', message);
     })
 })
 
