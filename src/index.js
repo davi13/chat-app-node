3d7de3b376fd67fe5641a47730c851de8c00b90b
Daviel Frias Garcia <yfriasgarcia@gmail.com>
2019-09-27 20:47:33 +0200

    working whith date an put timeStamp to the messages V3

--- src/index.js
+++ src/index.js
@@ -18,14 +18,14 @@ io.on('connection', (socket) => {
     console.log('New Websocket connection');
 
     socket.emit('message', generateMessage('Welcome'));
-    socket.broadcast.emit('message', 'A new user has joined!');
+    socket.broadcast.emit('message', generateMessage('A new user has joined!'));
 
     socket.on('sendMessage', (message, callback) => {
         const filter = new Filter()
         if (filter.isProfane(message)) {
             return callback('Profanity is not allowed!')
         }
-        io.emit('message', message);
+        io.emit('message', generateMessage(message));
         callback();
     });
 
@@ -37,7 +37,7 @@ io.on('connection', (socket) => {
 
     });
     socket.on('disconnect', () => {
-        io.emit('message', 'A user has left!')
+        io.emit('message', generateMessage('A user has left!'))
     });
 
 })
