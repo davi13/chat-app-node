a26611738315aac865b889d7234e703257f24612
Daviel Frias Garcia <yfriasgarcia@gmail.com>
2019-09-29 16:04:53 +0200

    cretion of function called addUser that allow us to save the user, idUser end the room in array

---
+++ src/utils/users.js
@@ -0,0 +1,29 @@
+const users = [];
+//addUser, removeUser, getUsers, getUSerInroom
+const addUser = ({ id, username, room }) => {
+    //Clean th data
+    username = username.trim().toLowerCase();
+    room = room.trim().toLowerCase();
+
+    //Validete the data
+    if (!username || room) {
+        return {
+            error: 'Username and room are required!'
+        }
+    }
+
+    //check for existing user
+    const existingUser = users.find((user) => {
+        return user.room === room && user.username === username
+    });
+    //Validate username
+    if (existingUser) {
+        return {
+            error: 'Username is in use!'
+        }
+    }
+    //store user
+    const user = { id, username, room }
+    users.push(user);
+    return { user }
+}
