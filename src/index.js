4d2af51d74bcbdf113b0180c05f8dd043e04950d
Daviel Frias Garcia <yfriasgarcia@gmail.com>
2019-10-01 09:11:39 +0200

    index.js: showing the room name for every user that joing that room V2

--- src/index.js
+++ src/index.js
@@ -60,7 +60,7 @@ io.on('connection', (socket) => {
             io.to(user.room).emit('roomData', {
                 room: user.room,
                 users: getUsersInRoom(user.room)
-            })
+            });
         }
     });
 })
